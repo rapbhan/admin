32dd3d6196d9aa5e85cd2bce96b2a2e849c359f1
Paddy <rapbhan@gmail.com>
2017-07-21 06:27:06 +0000

    Revert "Changed the Label to Title"
    This reverts commit e7f498948ae451cf55783371f06ba0b11d6a6e32.

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,9 +2,9 @@ class Product < ActiveRecord::Base
   rails_admin do
     list do
       items_per_page 3
-      field :name do
-        label "Title"
-      end
+      # field :name do
+      #   label "Title"
+      # end
     end
   end
 end
